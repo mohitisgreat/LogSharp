67f0bbaa6f6f1dde47f1f782521f95cfa92ce6ee
Mohit Agrawal <aaponkastylemeramohit@gmail.com>
2018-03-29 15:39:52 +0530

    Added the new appender FileAppender in the namespace LogSharp.Appender

---
+++ LogSharp/Appenders/FileAppender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogSharp.Appenders
+{
+    public class FileAppender : ILogAppender
+    {
+        public void AppendLog(LogSeverity logSeverity, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
