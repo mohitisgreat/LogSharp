254ce13a4756f19bf603cfa3c213600278590ed4
Mohit Agrawal <aaponkastylemeramohit@gmail.com>
2018-03-29 19:12:18 +0530

    Added an example for file logging.

---
+++ Examples/FileLogging/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+using LogSharp;
+using LogSharp.Appenders;
+
+namespace FileLogging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize the log manager.
+            LogManager.Initialize();
+            string logPattern = "{hour}:{minute}:{second} {meridian}  {file}, {line} {severity}: {message}";
+            LogManager.Instance.LogPattern = logPattern;
+
+            // Add the appenders.
+            LogManager.Instance.AddAppender(new ConsoleAppender());
+
+            FileAppender fileAppender = new FileAppender(Directory.GetCurrentDirectory(), "application_{hour}_{minute}_{second}.log");
+            LogManager.Instance.AddAppender(fileAppender);
+
+            // Now, Initialization is completed, Intialization should be done in the startup.
+            Logger.Trace("This is a trace message");
+            Logger.Info("This is a info message");
+        }
+    }
+}
