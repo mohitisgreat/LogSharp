839feb93ff9aaef82517cd9b6663d229a9250e3f
Mohit Agrawal <aaponkastylemeramohit@gmail.com>
2018-03-30 14:15:18 +0530

    Added a new example to demonstrate LogSharp.Appenders.RollingFileAppender

---
+++ Examples/RollFileLogging/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+
+using LogSharp;
+using LogSharp.Appenders;
+
+namespace RollFileLogging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InitializeLogging();
+
+            for (int x = 0; x < 1000; ++x) {
+                Logger.Warning($"X is at {x}");
+            }
+        }
+
+        static void InitializeLogging()
+        {
+            LogManager.Initialize();
+
+            string logPattern = "[{hour}:{minute}:{second} {meridian}] {severity}: {message}";
+            LogManager.Instance.LogPattern = logPattern;
+
+            string directory = Path.Combine(Directory.GetCurrentDirectory(), "Log");
+            if (!Directory.Exists(directory)) {
+                Directory.CreateDirectory(directory);
+            }
+
+            RollingFileAppender fileAppender = new RollingFileAppender(1024, "application_{filenumber}.log", directory);
+
+            LogManager.Instance.AddAppender(fileAppender);
+            LogManager.Instance.AddAppender(new ConsoleAppender());
+        }
+    }
+}
